80e382f27c005dc3dfa5e84bd96f1b440fc78d17
afsaberi <afsaberi23@gmail.com>
2015-09-13 19:27:06 -0400

    this is the start
    weqwe

---
+++ test/test/Mainform.vb
@@ -0,0 +1,25 @@
+﻿Public Class Mainform
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim clock As Date
+        Dim ww As Timer
+        clock = Now
+        Time.Text = clock
+
+        ww.Enabled = True
+        MsgBox(ww)
+
+    End Sub
+
+    Private Sub Clock_Click(sender As Object, e As EventArgs) Handles Clock.Click
+
+    End Sub
+
+    Public Sub Time_Click(sender As Object, e As EventArgs) Handles Time.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("gtfo")
+    End Sub
+End Class
